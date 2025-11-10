e0f9d8498be065168aff009cfb1c33e05275f398
Braja Gopal Das <142829323+Braja4u@users.noreply.github.com>
2025-11-11 00:17:03 +0530

    Updated Day7-Challenge.sql

--- Day7-Challenge.sql
+++ Day7-Challenge.sql
@@ -1,27 +1,34 @@
--- Topics: GROUP BY, aggregating by categories
 
--- ### Practice Questions:
--- 1. Count the number of patients by each service.
-select service, count(*) as num_patients from patients
-group by service;
--- 2. Calculate the average age of patients grouped by service.
-select service, ROUND(avg(age),2) as avg_age from patients
-group by service;
--- 3. Find the total number of staff members per role.
-select role, count(*) as total_num_staff from staff
-group by role;
+-- Topics: HAVING clause, filtering aggregated results
+
+### Practice Questions:
+-- 1. Find services that have admitted more than 500 patients in total.
+select service, sum(patients_admitted) as patients_count from services_weekly
+group by service 
+having patients_count > 500 ; 
+-- 2. Show services where average patient satisfaction is below 75.
+select service, avg(patient_satisfaction) as avg_patients_satisfaction from services_weekly
+group by service 
+having avg(patient_satisfaction) < 75 ; 
+-- 3. List weeks where total staff presence across all services was less than 50.
+select week, sum(present) as total_staff_present from staff_schedule
+group by week 
+having total_staff_present < 50 ; 
 
 
 -- ### Daily Challenge:
+-- Identify services that refused more than 100 patients in total and had an average patient satisfaction below 80. 
+-- Show service name, total refused, and average satisfaction.
+SELECT service, ROUND(SUM(patients_refused),2) AS total_patients_refused, 
+ROUND(AVG(patient_satisfaction),2) AS avg_patient_satisfaction
+FROM services_weekly
+GROUP BY service
+HAVING total_patients_refused > 100 AND avg_patient_satisfaction < 80;
 
--- For each hospital service, calculate the total number of patients admitted, total patients refused, 
--- and the admission rate (percentage of requests that were admitted). Order by admission rate descending.
-select service, sum(patients_admitted) as total_patients_admitted, 
-sum(patients_refused) as total_patients_refused, 
-ROUND((sum(patients_admitted)/sum(patients_refused))*100,2) as admission_rate
-from services_weekly
-group by service
-order by admission_rate desc;
+
+
+
+
 
 
 
